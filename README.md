4ce6ed394c92c73c35e5a7f7b48a89da7f63801d
scammi <scammi@gmail.com>
2020-03-23 00:59:20 -0300

    ICD-app

--- README.md
+++ README.md
@@ -3,6 +3,9 @@ ICD-10 for your application.
 
 This small project consists of the ICD-10 as a CVS file and a PHP script that takes that file and creates an array. You can use this array in your application, for example, to create a SQL table.
 
-[ICD code as a text file ](https://www.cms.gov/Medicare/Coding/ICD10/2020-ICD-10-CM)
+When oppening the CVS file, the colums but be separeted by the "$" caracter.
 
+[ICD code as a text file ](https://www.cms.gov/Medicare/Coding/ICD10/2020-ICD-10-CM)
 [More information on ICD](https://www.who.int/classifications/icd/en/)
+
+[PHP code goes to])(https://phpenthusiast.com/blog/parse-csv-with-php)
---
+++ icd-array.php
@@ -0,0 +1,27 @@
+<?php
+
+$filename = 'icd10cm_codes_2020.csv';
+
+// The nested array to hold all the arrays
+$the_big_array = [];
+
+// Open the file for reading
+if (($h = fopen("{$filename}", "r")) !== FALSE)
+{
+  // Each line in the file is converted into an individual array that we call $data
+  // The items of the array are comma separated
+  while (($data = fgetcsv($h, 1000, "$")) !== FALSE)
+  {
+    // Each individual array is being pushed into the nested array
+    $data[1] = trim($data[1]);
+    $the_big_array[] = $data;
+  }
+
+  // Close the file
+  fclose($h);
+}
+
+// Display the code in a readable format
+echo "<pre>";
+var_dump($the_big_array);
+echo "</pre>";
